34fc5d8ed0dc8f5321c40b8e886e60555192eb61
theShihamAhamed <theShihamAhamed@gmail.com>
2025-12-31 13:16:38 +0530

    Initial commit: Spring Boot project setup with MongoDB and AWS S3 configuration placeholders

---
+++ src/main/java/com/dev/BiteNowAPI/BiteNowApiApplication.java
@@ -0,0 +1,13 @@
+package com.dev.BiteNowAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiteNowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiteNowApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/BiteNowAPI/BiteNowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.BiteNowAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiteNowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
